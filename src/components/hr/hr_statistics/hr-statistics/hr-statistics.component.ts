506b2b83d81aed7211c175c5eee0183c7a683bca
Akram <akrambenaoun1993@gmail.com>
2025-05-02 11:37:49 +0100

    components updates

---
+++ src/components/hr/hr_statistics/hr-statistics/hr-statistics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-statistics',
+  imports: [],
+  templateUrl: './hr-statistics.component.html',
+  styleUrl: './hr-statistics.component.css'
+})
+export class HrStatisticsComponent {
+
+}
