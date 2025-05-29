b6f0d6417af85ab2714221557c78fc9d758708fd
Akram <akrambenaoun1993@gmail.com>
2025-05-29 18:31:30 +0100

    add data loading spinner + other changes

--- src/components/planning/view-project/view-project.component.ts
+++ src/components/planning/view-project/view-project.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ProjectsService } from '../../../services/projects.services';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-project',
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressSpinner],
   templateUrl: './view-project.component.html',
   styleUrl: './view-project.component.css',
 })
@@ -19,34 +21,30 @@ export class ViewProjectComponent {
     private router: Router
   ) {}
   ngOnInit() {
-    const project_id = this.route.snapshot.paramMap.get('id');
-    console.log('Project ID:: ', project_id);
+    const project_id = Number(this.route.snapshot.paramMap.get('id'));
 
     if (project_id !== null) {
-      this.getProjectById(Number(project_id));
-    } else {
-      console.error('Project ID is null');
+      this.getProjectById(project_id);
     }
   }
 
   getProjectById(project_id: number) {
     this.isLoading = true;
-    this.projectsService.getProjectById(project_id).subscribe({
-      next: (data: any) => {
+    this.projectsService.getProjectById(project_id).subscribe((data: any) => {
+      if(data.success) {
         this.isLoading = false;
-        if (data.success) {
-          this.project_data = data.data;
-          console.log('Project details:', this.project_data);
-        } else {
-          console.error('Error fetching project details:', data.message);
-        }
-      },
-      error: (err) => {
+        this.project_data = data.data;
+      }else {
         this.isLoading = false;
-        console.error('API error:', err);
-      },
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: data.message || 'une erreur s\'est produite lors de la récupération du projet.',
+        });
+      }
     });
   }
+
   goBack(): void {
     this.router.navigate(['/main-page/planning/show-project']);
   }
