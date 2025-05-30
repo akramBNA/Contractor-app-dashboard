baea7e5802e9ec921cf1dc1ab8c09fd4239ac5cc
Akram <akrambenaoun1993@gmail.com>
2025-05-30 21:28:07 +0100

    create missions_list component

---
+++ src/components/missions/missions-list/missions-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missions-list',
+  imports: [],
+  templateUrl: './missions-list.component.html',
+  styleUrl: './missions-list.component.css'
+})
+export class MissionsListComponent {
+
+}
