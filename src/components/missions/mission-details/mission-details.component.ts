ca75dc0c234e092e710a0c7443eeae838e446cfd
Akram <akrambenaoun1993@gmail.com>
2025-06-02 22:23:32 +0100

    changes

--- src/components/missions/mission-details/mission-details.component.ts
+++ src/components/missions/mission-details/mission-details.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MissionsService } from '../../../services/missions.services';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-mission-details',
@@ -45,27 +51,44 @@ export class MissionDetailsComponent implements OnInit {
 
   getMissionDetails(missionId: string) {
     this.isLoading = true;
-    this.missionsService.getMissionById(missionId).subscribe((response: any) => {
-      this.isLoading = false;
-      if (response.success) {
-        const mission = response.data;
-        this.missionForm.patchValue({
-          mission_name: mission.mission_name,
-          mission_description: mission.mission_description,
-          start_at: mission.start_at,
-          end_at: mission.end_at,
-          priority: mission.priority,
-          expenses: mission.expenses,
-        });
-      } else {
-        console.error('No mission found', response.message);
-      }
-    });
+    this.missionsService
+      .getMissionById(missionId)
+      .subscribe((response: any) => {
+        this.isLoading = false;
+        if (response.success) {
+          const mission = response.data;
+          this.missionForm.patchValue({
+            mission_name: mission.mission_name,
+            mission_description: mission.mission_description,
+            start_at: mission.start_at,
+            end_at: mission.end_at,
+            priority: mission.priority,
+            expenses: mission.expenses,
+          });
+        } else {
+          console.error('No mission found', response.message);
+        }
+      });
   }
 
   updateMission() {}
 
-  downloadPdf() {}
+  downloadPDF(): void {
+    const element = document.getElementById('pdfContent');
+    console.log("element", element);
+    
+    const options = {
+      margin: 0.5,
+      filename: 'mission-details.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' },
+    };
+
+    if (element) {
+      html2pdf().from(element).set(options).save();
+    }
+  }
 
   goBack() {
     this.router.navigate(['/main-page/missions/missions-list']);
