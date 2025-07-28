070dcd00a813f0d98936fce0c80ad8aeb020fef8
Akram <akrambenaoun1993@gmail.com>
2025-07-28 13:53:59 +0100

    update: handle no data case scenario on keyword search

--- src/components/hr/employee-list/employee-list.component.ts
+++ src/components/hr/employee-list/employee-list.component.ts
@@ -28,7 +28,7 @@ import { MatInputModule } from '@angular/material/input';
     LoadingSpinnerComponent,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
   ],
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -36,6 +36,7 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class EmployeeListComponent implements OnInit {
   isLoading: boolean = false;
+  isEmpty: boolean = false;
 
   limit: number = 20;
   offset: number = 0;
@@ -48,72 +49,87 @@ export class EmployeeListComponent implements OnInit {
   new_employees_count: number = 0;
 
   constructor(
-    private employeeServicee: EmployeesService, 
+    private employeeServicee: EmployeesService,
     private router: Router,
-    private swalService: SwalService,
+    private swalService: SwalService
   ) {}
 
   ngOnInit(): void {
-    this.getAllEmployeesFunction(this.limit, this.offset, this.keyword.value ?? '');
+    this.getAllEmployeesFunction( this.limit, this.offset, this.keyword.value ?? '');
 
-    this.keyword.valueChanges.pipe(debounceTime(500),distinctUntilChanged()).subscribe((value: string | null) => {
-      this.offset = 0;
-      this.getAllEmployeesFunction(this.limit, this.offset, (value ?? '').trim());
-    });
+    this.keyword.valueChanges.pipe(debounceTime(500), distinctUntilChanged()).subscribe((value: string | null) => {
+        this.offset = 0;
+        this.getAllEmployeesFunction(
+          this.limit,
+          this.offset,
+          (value ?? '').trim()
+        );
+      });
   }
 
   getAllEmployeesFunction(lim: number, off: number, key: string) {
+    this.isEmpty = false;
     this.isLoading = true;
     this.employeeServicee.getAllEmployees(lim, off, key).subscribe((data: any) => {
-      if (data.success) {
-        this.isLoading = false;
-        this.employeeList = data.data;
-        this.total_employees_count = data.statistics.total;
-        this.male_employees_count = data.statistics.male;
-        this.female_employees_count = data.statistics.female;
-        this.new_employees_count = data.statistics.newEmployees;
-      }
-    });
+        if (data.success) {
+          if (data.data.length === 0) {
+            this.isEmpty = true;
+            this.employeeList = [];
+          }
+          this.isLoading = false;
+          this.employeeList = data.data;
+          this.total_employees_count = data.statistics.total;
+          this.male_employees_count = data.statistics.male;
+          this.female_employees_count = data.statistics.female;
+          this.new_employees_count = data.statistics.newEmployees;
+        }
+      });
   }
 
   onPageChange(event: PageEvent) {
     this.limit = event.pageSize;
     this.offset = event.pageIndex * event.pageSize;
-    this.getAllEmployeesFunction(this.limit, this.offset, this.keyword.value ?? '');
+    this.getAllEmployeesFunction( this.limit, this.offset, this.keyword.value ?? '');
   }
 
   onEditEmployee(employeeId: number) {
     this.router.navigate(['/main-page/hr/edit-employee', employeeId]);
   }
 
-onSearch() {
-  this.offset = 0;
-  this.getAllEmployeesFunction(this.limit, this.offset, this.keyword.value ?? '');
-}
+  onSearch() {
+    this.offset = 0;
+    this.getAllEmployeesFunction( this.limit, this.offset, this.keyword.value ?? '');
+  }
 
-clearSearch() {
-  this.keyword.setValue('');
-  this.offset = 0;
-  this.getAllEmployeesFunction(this.limit, this.offset, '');
-}
-
+  clearSearch() {
+    this.keyword.setValue('');
+    this.offset = 0;
+    this.getAllEmployeesFunction(this.limit, this.offset, '');
+  }
 
   onDeleteEmployee(employeeId: number) {
-    this.swalService.showConfirmation('Êtes-vous sûr de vouloir supprimer cet employé ?').then((result) => {
-      if (result.isConfirmed) {
-        this.isLoading = true;
-        this.employeeServicee.deleteEmployee(Number(employeeId)).subscribe((data: any) => {
-          if (data.success) {
-            this.isLoading = false;
-            this.swalService.showSuccess('L\'employé a été supprimé avec succès.').then(() => {
-              this.getAllEmployeesFunction(this.limit, this.offset, this.keyword.value ?? '');
+    this.swalService
+      .showConfirmation('Êtes-vous sûr de vouloir supprimer cet employé ?').then((result) => {
+        if (result.isConfirmed) {
+          this.isLoading = true;
+          this.employeeServicee.deleteEmployee(Number(employeeId)).subscribe((data: any) => {
+              if (data.success) {
+                this.isLoading = false;
+                this.swalService.showSuccess("L'employé a été supprimé avec succès.").then(() => {
+                    this.getAllEmployeesFunction(
+                      this.limit,
+                      this.offset,
+                      this.keyword.value ?? ''
+                    );
+                  });
+              } else {
+                this.isLoading = false;
+                this.swalService.showError(
+                  "Une erreur s'est produite lors de la suppression de l'employé."
+                );
+              }
             });
-          } else {
-            this.isLoading = false;
-            this.swalService.showError('Une erreur s\'est produite lors de la suppression de l\'employé.');
-          }
-        });
-      }
-    });
+        }
+      });
   }
 }
