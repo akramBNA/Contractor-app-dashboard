ace57e897026616d7ef2da450df5d77df6c8b441
Akram <akrambenaoun1993@gmail.com>
2025-07-30 15:05:40 +0100

    create a request leaves component

---
+++ src/components/leaves/request-leaves/request-leaves/request-leaves.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-leaves',
+  imports: [],
+  templateUrl: './request-leaves.component.html',
+  styleUrl: './request-leaves.component.css'
+})
+export class RequestLeavesComponent {
+
+}
