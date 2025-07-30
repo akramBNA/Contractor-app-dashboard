2dc349cc56979ed615fd14fea0926b637f36e1db
Akram <akrambenaoun1993@gmail.com>
2025-07-30 17:16:22 +0100

    create leave types services

--- src/components/hr/leaves/request-leaves/request-leaves/request-leaves.component.ts
+++ src/components/hr/leaves/request-leaves/request-leaves/request-leaves.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../../../shared/loading-spinner/loading-spinner.component';
+import { MatInputModule } from '@angular/material/input';
+import { h } from '../../../../../../node_modules/@angular/material/module.d-CyLvt0Fz';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { LeavesService } from '../../../../../services/leaves.services';
+import { SwalService } from '../../../../../shared/Swal/swal.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-request-leaves',
-  imports: [],
+  imports: [
+    LoadingSpinnerComponent,
+    MatInputModule,
+    h,
+    MatAutocompleteModule,
+    MatDatepicker,
+  ],
   templateUrl: './request-leaves.component.html',
-  styleUrl: './request-leaves.component.css'
+  styleUrl: './request-leaves.component.css',
 })
 export class RequestLeavesComponent {
+  isLoading: boolean = false;
+  requestLeavesForm: FormGroup;
 
+  constructor(
+    private leaveServices: LargestContentfulPaint,
+    private swalService: SwalService,
+    private fb: FormBuilder
+  ) {
+    this.requestLeavesForm = this.fb.group({
+      employee_id: [0, Validators.required],
+      leave_type_id: [0, Validators.required],
+      description: ['', Validators.required],
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(){
+    
+  }
 }
