363e8d6fb0cfa69fa46d1ca34fd74a0544140a0b
Akram <akrambenaoun1993@gmail.com>
2025-07-31 15:20:27 +0100

    add a holidays services

---
+++ src/services/holidays.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class HolidaysService {
+  constructor(private http: HttpClient) {}
+
+  readonly base_url = environment.backendURL;    
+
+    getAllHolidays(): Observable<any> {
+      return this.http.get<any>(`${this.base_url}/holidays/getAllHolidays/`);
+    };
+    addHoliday(holiday_data: any): Observable<any> {
+      return this.http.post<any>(`${this.base_url}/holiday_datas/addHoliday/`, holiday_data);
+    }
+}
