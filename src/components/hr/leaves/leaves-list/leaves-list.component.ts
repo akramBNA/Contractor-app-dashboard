490fee3e64fc173ab46814725e8f700f3006c389
Akram <akrambenaoun1993@gmail.com>
2025-08-01 17:43:11 +0100

    create a new leaves_list component

---
+++ src/components/hr/leaves/leaves-list/leaves-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaves-list',
+  imports: [],
+  templateUrl: './leaves-list.component.html',
+  styleUrl: './leaves-list.component.css'
+})
+export class LeavesListComponent {
+
+}
