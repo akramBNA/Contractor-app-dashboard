bc04969e19a899fb92d0a133c60ce71998373903
Akram <akrambenaoun1993@gmail.com>
2025-08-01 17:55:37 +0100

    create a new leaves_list component #2

--- src/components/hr/leaves/leaves-list/leaves-list.component.ts
+++ src/components/hr/leaves/leaves-list/leaves-list.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../../shared/loading-spinner/loading-spinner.component';
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { CommonModule } from '@angular/common';
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @Component({
   selector: 'app-leaves-list',
-  imports: [],
+  imports: [CommonModule, LoadingSpinnerComponent, MatInputModule, MatIconModule, MatPaginatorModule],
   templateUrl: './leaves-list.component.html',
   styleUrl: './leaves-list.component.css'
 })
 export class LeavesListComponent {
+  isLoading: boolean = false;
+  isEmpty: boolean = false;
+  leaves_data: any[] = [];
+  total_count: number = 0;
+  limit: number = 20;
+  offset: number = 0;
+  constructor() {}
 
+  ngOnInit() {}
+
+  onPageChange(event:any){}
+
+  acceptLeave(employeeId: number, leaveId: number) {
+  };
+
+  rejectLeave(employeeId: number, leaveId: number) {};
 }
