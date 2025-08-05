6a705eddae9c43f1392952fa0ffa23859b73618f
Akram <akrambenaoun1993@gmail.com>
2025-08-05 16:42:16 +0100

    create a role dialoge ts

--- src/components/settings/role-dialog/role-dialog.component.ts
+++ src/components/settings/role-dialog/role-dialog.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-role-dialog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatSelectModule, MatButtonModule],
   templateUrl: './role-dialog.component.html',
-  styleUrl: './role-dialog.component.css'
 })
 export class RoleDialogComponent {
+  roleForm: FormGroup;
+  roles = [
+    { label: 'Super Admin', id: 1 },
+    { label: 'Admin', id: 2 },
+    { label: 'Utilisateur', id: 3 },
+  ];
 
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<RoleDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { user_id: number; current_role: number }
+  ) {
+    this.roleForm = this.fb.group({
+      user_role_id: [data.current_role, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.roleForm.valid) {
+      this.dialogRef.close(this.roleForm.value.user_role_id);
+    }
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
