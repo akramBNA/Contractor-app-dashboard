c0397f3eb72db59d7881308089c74e5ced82d161
Akram <akrambenaoun1993@gmail.com>
2025-09-09 17:55:21 +0100

    create vehicles services

---
+++ src/services/vehicles.services.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class VehiclesService {
+  constructor(private http: HttpClient) {}
+
+  readonly base_url = environment.backendURL;    
+
+    getAllVehicles(limit: number, offset: number, keyword: string): Observable<any> {
+      return this.http.get<any>(`${this.base_url}/vehicles/getAllVehicles/${JSON.stringify({ limit: limit, offset: offset, keyword: keyword })}`);
+    };
+}
