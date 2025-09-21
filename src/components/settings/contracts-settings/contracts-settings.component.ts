6aaa657a6ea6ab684227290ad3dcaf218b5cf0c6
Akram <akrambenaoun1993@gmail.com>
2025-09-21 22:15:41 +0100

    add contract types TS

--- src/components/settings/contracts-settings/contracts-settings.component.ts
+++ src/components/settings/contracts-settings/contracts-settings.component.ts
@@ -5,6 +5,8 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ContractTypesService } from '../../../services/contract_types.services';
+import { SwalService } from '../../../shared/Swal/swal.service';
 
 @Component({
   selector: 'app-contracts-settings',
@@ -23,8 +25,35 @@ export class ContractsSettingsComponent {
   active_contracts_count: number = 0;
   inactive_contracts_count: number = 0;
 
+  constructor(
+    private contractTypesService: ContractTypesService,
+    private swalService: SwalService
+  ) {}
+
   ngOnInit(): void {
-    // Initialization logic here
+    this.getContractTypes();
+  };
+
+  getContractTypes(): void {
+    this.isLoading = true;
+    this.contractTypesService.getAllContractTypes().subscribe({
+      next: (response) => {
+        if(response.success){
+          this.contract_types_data = response.data;
+          this.overall_count = response.data.length;
+          this.isLoading = false;
+        } else {
+          this.contract_types_data = [];
+          this.overall_count = 0;
+          this.isEmpty = true;
+          this.isLoading = false;
+        }
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.swalService.showError('Erreur', 'Une erreur est survenue lors de la récupération des types de contrat.');
+      }
+    });
   };
 
   onEditContractType(contractTypeId: number): void {
