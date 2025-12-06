b09127c141a1a07f193355c5e57c3c077ccdce94
Akram <akrambenaoun1993@gmail.com>
2025-12-06 19:14:26 +0100

    Update edit-holidays component

--- src/components/hr/holidays/edit-holidays/edit-holidays.component.ts
+++ src/components/hr/holidays/edit-holidays/edit-holidays.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField, MatLabel } from "@angular/material/input";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-holidays',
-  imports: [],
+  imports: [CommonModule, MatFormField, MatLabel, ReactiveFormsModule],
   templateUrl: './edit-holidays.component.html',
   styleUrl: './edit-holidays.component.css'
 })
 export class EditHolidaysComponent {
+  editForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<EditHolidaysComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+
+    this.editForm = this.fb.group({
+      holiday_name: [data.holiday.holiday_name, [Validators.required, Validators.minLength(2)]],
+      holiday_date: [data.holiday.holiday_date, Validators.required]
+    });
+  }
+
+  close() {
+    this.dialogRef.close(false);
+  }
+
+  save() {
+    if (this.editForm.invalid) return;
+
+    this.dialogRef.close({
+      ...this.editForm.value
+    });
+  }
 }
