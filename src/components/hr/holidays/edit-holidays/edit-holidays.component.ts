85c8f6dabc4ea1353795e2023eaaa6cc8c36f197
Akram <akrambenaoun1993@gmail.com>
2025-12-06 19:10:57 +0100

    Create a new edit-holidays component

---
+++ src/components/hr/holidays/edit-holidays/edit-holidays.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-holidays',
+  imports: [],
+  templateUrl: './edit-holidays.component.html',
+  styleUrl: './edit-holidays.component.css'
+})
+export class EditHolidaysComponent {
+
+}
