20c470511d0499e3ce1e9c59dd54ea3eb106143c
Akram <akrambenaoun1993@gmail.com>
2025-12-20 23:08:41 +0100

    changes

--- src/services/contract_types.services.ts
+++ src/services/contract_types.services.ts
@@ -13,12 +13,4 @@ export class ContractTypesService {
       return this.http.get<any>(`${this.base_url}/contract_types/getAllContractTypes/`);
     };
 
-    updateContractType(contractType: any, id: any): Observable<any> {
-      return this.http.put<any>(`${this.base_url}/contract_types/updateContractType/`, contractType);
-    };
-
-    deleteContractType(id: any): Observable<any> {
-      return this.http.delete<any>(`${this.base_url}/contract_types/deleteContractType/${id}`);
-    };
-
 }
