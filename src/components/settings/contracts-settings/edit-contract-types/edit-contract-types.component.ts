b208a7160054bcf36fced5970967442b900d853f
Akram <akrambenaoun1993@gmail.com>
2026-01-14 16:25:36 +0100

    create a new edit-contract-types component (POPUP MODAL)

---
+++ src/components/settings/contracts-settings/edit-contract-types/edit-contract-types.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-contract-types',
+  imports: [],
+  templateUrl: './edit-contract-types.component.html',
+  styleUrl: './edit-contract-types.component.css'
+})
+export class EditContractTypesComponent {
+
+}
