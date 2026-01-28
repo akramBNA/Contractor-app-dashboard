460af3331c804068e38819f22b99ec8b03cc9750
Akram <akrambenaoun1993@gmail.com>
2026-01-28 23:23:25 +0100

    create an add contract type modal (TS)

--- src/components/settings/contracts-settings/add-contract-types/add-contract-types.component.ts
+++ src/components/settings/contracts-settings/add-contract-types/add-contract-types.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { ContractTypesService } from '../../../../services/contract_types.services';
+import { SwalService } from '../../../../shared/Swal/swal.service';
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
-  selector: 'app-add-contract-types',
-  imports: [],
+  selector: 'app-contract-type-form-dialog',
   templateUrl: './add-contract-types.component.html',
-  styleUrl: './add-contract-types.component.css'
+  imports: [CommonModule, MatDialogModule, MatSelectModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
-export class AddContractTypesComponent {
+export class ContractTypeFormDialogComponent {
+  form!: FormGroup;
+  isLoading = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private contractTypesService: ContractTypesService,
+    private swalService: SwalService,
+    private dialogRef: MatDialogRef<ContractTypeFormDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.form = this.fb.group({
+      contract_name: [data.contract_name, Validators.required],
+      leaves_credit: [data.leaves_credit, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  submit(): void {
+    if (this.form.invalid) return;
+
+    this.isLoading = true;
+
+    this.contractTypesService.addContractType(this.form.value).subscribe({
+        next: (response) => {
+          this.isLoading = false;
+
+          if (response.success) {
+            this.swalService.showSuccess('Un nouveau type de contrat est ajouté avec succès.');
+
+            this.dialogRef.close(response.data);
+          } else {
+            this.swalService.showError('Erreur', response.message || 'Ajout impossible.');
+          }
+        },
+        error: () => {
+          this.isLoading = false;
+          this.swalService.showError('Erreur','Une erreur est survenue.');
+        }
+      });
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
