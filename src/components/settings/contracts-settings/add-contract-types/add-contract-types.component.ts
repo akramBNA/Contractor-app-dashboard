22cbed6bea6a84580e92948a234c2d62bf253f57
Akram <akrambenaoun1993@gmail.com>
2026-01-28 23:18:26 +0100

    create a new add contract types component

---
+++ src/components/settings/contracts-settings/add-contract-types/add-contract-types.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contract-types',
+  imports: [],
+  templateUrl: './add-contract-types.component.html',
+  styleUrl: './add-contract-types.component.css'
+})
+export class AddContractTypesComponent {
+
+}
