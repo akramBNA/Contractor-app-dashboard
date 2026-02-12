c3ddf4373719e6cb23bed815da97be9884e41109
Akram <akrambenaoun1993@gmail.com>
2026-02-12 20:39:39 +0100

    prodection settings

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     backendURL: 'https://contractor-management-backend-bvll.onrender.com/api',
+    socketURL: 'http://localhost:3000',
   };
   
--- src/services/socket.services.ts
+++ src/services/socket.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environments.prod';
 
 @Injectable({
   providedIn: 'root',
