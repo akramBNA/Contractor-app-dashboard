3d195cd01e7fd55a74fd8d3f60724a92515e3b70
Akram <akrambenaoun1993@gmail.com>
2026-04-30 22:04:51 +0100

    CREATE a add-bills component

---
+++ src/components/bills/add-bills/add-bills.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-bills',
+  imports: [],
+  templateUrl: './add-bills.component.html',
+  styleUrl: './add-bills.component.css'
+})
+export class AddBillsComponent {
+
+}
