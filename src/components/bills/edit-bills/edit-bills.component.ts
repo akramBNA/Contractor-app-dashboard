5253023bee54dadc6d1c6d79ed6a5fa9ec529118
Akram <akrambenaoun1993@gmail.com>
2026-04-30 22:05:43 +0100

    CREATE a edit-bills component

---
+++ src/components/bills/edit-bills/edit-bills.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-bills',
+  imports: [],
+  templateUrl: './edit-bills.component.html',
+  styleUrl: './edit-bills.component.css'
+})
+export class EditBillsComponent {
+
+}
